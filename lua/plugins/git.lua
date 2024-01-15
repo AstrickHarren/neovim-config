d719b2bea8305618dbe7a398e72b9a0e7ba5c42d
Astrick <astrick.harren.at.work@gmail.com>
2024-01-15 16:37:27 -0500

    refactor: organize git files & add neogit

---
+++ lua/plugins/git.lua
@@ -0,0 +1,68 @@
+return {
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {},
+		keys = {
+			{ "<leader>gr", "<cmd>Gitsigns reset_hunk<cr>", mode = { "n", "v" } },
+			{ "<leader>gp", "<cmd>Gitsigns preview_hunk_inline<cr>", mode = { "n", "v" } },
+			{ "<leader>gb", "<cmd>Gitsigns blame_line<cr>" },
+			{ "<leader>gs", "<cmd>Gitsigns stage_hunk<cr> " },
+			{ "<leader>gu", "<cmd>Gitsigns undo_stage_hunk<cr> " },
+
+			{ "]g", "<cmd>Gitsigns next_hunk<cr>", desc = "Next hunk", mode = { "n", "v" } },
+			{ "[g", "<cmd>Gitsigns prev_hunk<cr>", desc = "Previous hunk", mode = { "n", "v" } },
+		},
+		lazy = false,
+	},
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+			"nvim-telescope/telescope.nvim", -- optional
+		},
+		config = true,
+		keys = {
+			{ "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit panel", mode = { "n", "v" } },
+			{ "<leader>gc", "<cmd>Neogit commit<cr>", desc = "Git commit panel", mode = { "n", "v" } },
+		},
+	},
+	{
+		"sindrets/diffview.nvim",
+		opts = {
+			hooks = {
+				-- diff_buf_win_enter = function()
+				-- 	vim.opt_local.foldenable = false
+				-- end,
+				--
+			},
+		},
+		keys = {
+			{
+				"<leader>gd",
+				"<cmd>DiffviewOpen<cr>",
+				desc = "Open diffview",
+			},
+			{
+				"q",
+				"<cmd>DiffviewClose<cr>",
+				desc = "Close diffview",
+			},
+			{
+				"<esc>",
+				"<cmd>DiffviewClose<cr>",
+				desc = "Close diffview",
+			},
+			{
+				"<leader>gH",
+				"<cmd>DiffviewFileHistory<cr>",
+				desc = "All file history",
+			},
+			{
+				"<leader>gh",
+				"<cmd>DiffviewFileHistory %<cr>",
+				desc = "Current file history",
+			},
+		},
+	},
+}
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,52 +1,51 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 300
-	end,
-	opts = {
-		window = { margin = { 1, 0, 1, 0.8 }, border = "single" },
-		layout = { height = { min = 4, max = 75 } },
-	},
-	config = function(_, opts)
-		local wk = require("which-key")
-		wk.setup(opts)
-		wk.register({
-			mode = { "n", "v" },
-			["<leader>"] = {
-				g = { name = "+Git" },
-				t = { name = "+Terminal" },
-				d = { name = "+Diff" },
-				l = { name = "+Lsp" },
-				L = { "<cmd>Lazy<cr>", "Lazy" },
-				["?"] = { "<cmd>WhichKey<cr>", "Help" },
-			},
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+    end,
+    opts = {
+        window = { margin = { 1, 0, 1, 0.8 }, border = "single" },
+        layout = { height = { min = 4, max = 75 } },
+    },
+    config = function(_, opts)
+        local wk = require("which-key")
+        wk.setup(opts)
+        wk.register({
+            mode = { "n", "v" },
+            ["<leader>"] = {
+                g = { name = "+Git" },
+                t = { name = "+Terminal" },
+                l = { name = "+Lsp" },
+                L = { "<cmd>Lazy<cr>", "Lazy" },
+                ["?"] = { "<cmd>WhichKey<cr>", "Help" },
+            },
 
-			["<c-w>"] = { "<cmd>x<cr>", "Close window" },
-		})
+            ["<c-w>"] = { "<cmd>x<cr>", "Close window" },
+        })
 
-		wk.register({
-			mode = { "n", "x", "o" },
-			g = {
-				name = "+Goto",
-				["*"] = "Search forward under cursor (exprwise)",
-				["#"] = "Search backward under cursor (exprwise)",
-				h = { "0", "First character of line" },
-				s = { "^", "First non-blank character of line" },
-				l = { "g_", "Last character of line" },
-				e = { "G", "Last non-blank character of line" },
-			},
-		})
+        wk.register({
+            mode = { "n", "x", "o" },
+            g = {
+                name = "+Goto",
+                ["*"] = "Search forward under cursor (exprwise)",
+                ["#"] = "Search backward under cursor (exprwise)",
+                h = { "0", "First character of line" },
+                s = { "^", "First non-blank character of line" },
+                l = { "g_", "Last character of line" },
+                e = { "G", "Last non-blank character of line" },
+            },
+        })
 
-		-- wk.register({
-		-- 	mode = { "c" },
-		-- 	["<c-a>"] = { "<Home>", "Move to start of line" },
-		-- 	["<c-b>"] = { "<Left>", "Move left" },
-		-- 	["<c-f>"] = { "<Right>", "Move right" },
-		-- 	["<c-s>"] = { "<c-f>", "Move right" },
-		-- 	["<m-b>"] = { "<S-Left>", "Move left word" },
-		-- 	["<m-f>"] = { "<S-Right>", "Move right word" },
-		-- })
-	end,
+        -- wk.register({
+        -- 	mode = { "c" },
+        -- 	["<c-a>"] = { "<Home>", "Move to start of line" },
+        -- 	["<c-b>"] = { "<Left>", "Move left" },
+        -- 	["<c-f>"] = { "<Right>", "Move right" },
+        -- 	["<c-s>"] = { "<c-f>", "Move right" },
+        -- 	["<m-b>"] = { "<S-Left>", "Move left word" },
+        -- 	["<m-f>"] = { "<S-Right>", "Move right word" },
+        -- })
+    end,
 }
