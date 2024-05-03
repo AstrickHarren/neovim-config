8be0cadba1844521eb2d87afe122441a172e3a6b
Astrick <astrick.harren.at.work@gmail.com>
2024-05-02 20:52:17 -0400

    feat: dressing, nvim-surround, copilot

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,16 @@ require "nvchad.options"
 
 -- add yours here!
 
+vim.opt.swapfile = false
+vim.opt.signcolumn = "yes:3"
+vim.opt.fillchars = { diff = "╱", eob = " " }
+vim.opt.relativenumber = true
+vim.opt.diffopt:append "iwhiteall"
+
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,23 +1,38 @@
-return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    config = function()
-      require "configs.conform"
-    end,
-  },
+local M = {
+  "stevearc/conform.nvim",
+  "neovim/nvim-lspconfig",
+  "nvim-telescope/telescope.nvim",
+  { "zbirenbaum/copilot.lua", event = "InsertEnter" },
+  { "kylechui/nvim-surround", event = "VeryLazy", config = true },
+  { "stevearc/dressing.nvim", event = "VeryLazy", config = true },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require("nvchad.configs.lspconfig").defaults()
-      require "configs.lspconfig"
-    end,
-  },
-
-  {
-    "folke/which-key.nvim",
-    enabled = false,
-  },
+  -- disabled
+  { "folke/which-key.nvim", config = false, enabled = false },
 }
+
+-------------------- Loading plugins --------------------
+local function find_config(repo)
+  repo = repo:match "([^/]+)$"
+  repo = repo:match "([^-]+)$"
+  if repo:find "%." then
+    repo = repo:match "(.+)%..+"
+  end
+  return function()
+    require("configs." .. repo)
+  end
+end
+
+local function load_plugins(repos)
+  local plugins = {}
+  for i, repo in ipairs(repos) do
+    if type(repo) == "string" then
+      repo = { repo, config = find_config(repo) }
+    elseif repo.config == nil then
+      repo.config = find_config(repo[1])
+    end
+    plugins[i] = repo
+  end
+  return plugins
+end
+
+return load_plugins(M)
