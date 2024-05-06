a6034b253d44e608c4fc43feb98270caf9a982f8
Astrick <astrick.harren.at.work@gmail.com>
2024-05-06 11:11:11 -0400

    fix(cmp): `kind` comparator not concluding

--- lua/utils/comparators.lua
+++ lua/utils/comparators.lua
@@ -2,6 +2,13 @@ local compare = require "cmp.config.compare"
 local types = require "cmp.types"
 local M = {}
 
+local function conclude(diff)
+  if diff == 0 then
+    return nil
+  end
+  return diff < 0
+end
+
 M.reduce_keywords = function(a, b)
   if a:get_kind() == types.lsp.CompletionItemKind.Keyword then
     return false
@@ -14,23 +21,21 @@ end
 
 M.sort_label = function(a, b)
   local diff = vim.stricmp(a.completion_item.label, b.completion_item.label)
-  return diff == 0 and nil or diff < 0
+  return conclude(diff)
 end
 
 M.kind = function(entry1, entry2)
   local kind1 = entry1:get_kind() --- @type lsp.CompletionItemKind | number
   local kind2 = entry2:get_kind() --- @type lsp.CompletionItemKind | number
-  kind1 = kind1 == types.lsp.CompletionItemKind.Snippet and 1 or kind1
-  kind2 = kind2 == types.lsp.CompletionItemKind.Snippet and 1 or kind2
 
   kind1 = kind1 == types.lsp.CompletionItemKind.Text and 100 or kind1
   kind2 = kind2 == types.lsp.CompletionItemKind.Text and 100 or kind2
 
-  kind1 = kind1 == types.lsp.CompletionItemKind.Keyword and 200 or kind1
-  kind2 = kind2 == types.lsp.CompletionItemKind.Keyword and 200 or kind2
+  kind1 = kind1 == types.lsp.CompletionItemKind.Snippet and 1 or kind1
+  kind2 = kind2 == types.lsp.CompletionItemKind.Snippet and 1 or kind2
 
   local diff = kind1 - kind2
-  return diff == 0 and nil or diff < 0
+  return conclude(diff)
 end
 
 M = vim.tbl_extend("force", compare, M)
