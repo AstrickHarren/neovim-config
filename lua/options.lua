09280b084799dda046dae3230d9710343ab6a267
Astrick <astrick.harren.at.work@gmail.com>
2024-05-31 10:48:11 -0500

    feat(vim): fold with treesitter

--- lua/options.lua
+++ lua/options.lua
@@ -9,6 +9,12 @@ vim.opt.relativenumber = true
 vim.opt.diffopt:append "iwhiteall"
 vim.opt.ph = 10 -- for cmp use
 
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldlevel = 99
+vim.opt.foldtext = ""
+-- vim.opt.foldlevelmax = 4
+
 vim.o.autoread = true
 vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
   command = "if mode() != 'c' | checktime | endif",
